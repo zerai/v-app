b07454aa4fa4101aa90008cf381defa73508a7c6
zero <teclaizerai@gmail.com>
2025-04-05 00:07:34 +0200

    Init walking skeleton.

---
+++ service/config/bundles.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfonycasts\MicroMapper\SymfonycastsMicroMapperBundle::class => ['all' => true],
+];
---
+++ service/ecs.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\NamespaceNotation\BlankLineAfterNamespaceFixer;
+use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withCache(__DIR__ . '/var/cache_tools/ecs')
+    ->withPaths([
+        # __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        # __DIR__ . '/tools',
+    ])
+
+    ->withSkip([
+        __DIR__ . '/src/Kernel.php',
+        __DIR__ . '/tests/bootstrap.php',
+        BlankLineAfterOpeningTagFixer::class,
+    ])
+
+    // add a single rule
+    ->withRules([
+        BlankLineAfterNamespaceFixer::class,
+        DeclareStrictTypesFixer::class,
+        FullyQualifiedStrictTypesFixer::class,
+        NativeFunctionInvocationFixer::class,
+        NoUnusedImportsFixer::class,
+        OrderedImportsFixer::class,
+        StrictComparisonFixer::class,
+
+    ])
+
+    // add sets - group of rules
+    ->withPreparedSets(
+        arrays: true,
+        comments: true,
+        docblocks: true,
+        namespaces: true,
+        spaces: true,
+        psr12: true
+     )
+     
+     ;
