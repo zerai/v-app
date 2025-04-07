054783ab9628378925ca74f05205f4bc01ee41fa
zero <teclaizerai@gmail.com>
2025-04-07 10:29:24 +0200

    Init. application contexts, tools configuration.
    - ClinicManagement context
    - FrontDesk context
    - VetClinicPublic

---
+++ service/context/clinicManagement/tests/Integration/RemoveMePlaceholderTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace ClinicManagementTests\Integration;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class RemoveMePlaceholderTest extends TestCase
+{
+    public function test_remove_me(): void
+    {
+        Assert::assertEquals(4, 2 + 2);
+    }
+}
