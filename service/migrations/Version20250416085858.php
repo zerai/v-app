5138a9fb1c4b25f59009a645330616a5df6a39c2
zero <teclaizerai@gmail.com>
2025-04-16 11:00:43 +0200

    ClinicManagemet added migration file.

---
+++ service/migrations/Version20250416085858.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250416085858 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'ClinicManagement: add room table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE cm_room (id UUID NOT NULL, name VARCHAR(180) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN cm_room.id IS '(DC2Type:room_id)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN cm_room.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE fdoctor (id SERIAL NOT NULL, name VARCHAR(180) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN fdoctor.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE room (id UUID NOT NULL, name VARCHAR(150) NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN room.id IS '(DC2Type:uuid)'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE cm_room
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE fdoctor
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE room
+        SQL);
+    }
+}
